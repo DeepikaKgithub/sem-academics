8ea90960a47b59086ad04873b5c1dbe2c8a6b15d
DeepikaKgithub <143379956+DeepikaKgithub@users.noreply.github.com>
2024-01-29 15:56:47 +0530

    ds node.c

--- ds/ds node.c
+++ ds/ds node.c
@@ -1,87 +1,165 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-typedef struct 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Enode
 {
-    int usn;
+    char ssn[15];
     char name[20];
-    char branch[20];
-    int semester;
-    char phone[20];
-}student;
-struct node
+    char dept[5];
+    char designation[10];
+    int salary;
+    long long int phno;
+    struct Enode *left;
+    struct Enode *right;
+} *head = NULL, *tail = NULL, *temp1, *temp2;
+
+void create(char[], char[], char[], char[], int, long long int);
+void ins_beg(char[], char[], char[], char[], int, long long int);
+void ins_end(char[], char[], char[], char[], int, long long int);
+void del_beg();
+void del_end();
+void display();
+
+int main()
 {
-    int usn;
-    char name[20];
-    char branch [20];
-     int semester;
-    char phone[20];
-    struct node *link;
-};
-typedef struct node *Node;
-Node getnode()
-{
-    Node X;
-    X=(Node)malloc(size of (struct node));
-    if(x==Null)
+    int choice;
+    char s[15], n[20], dpt[5], des[10];
+    int sal;
+    long long int p;
+
+    printf("1.Create\n2.Display\n3.Insert at beginning\n4.Insert at End\n5.Delete at beginning\n6.Delete at End\n7.Exit\n");
+
+    while (1)
     {
-        printf("out of memory\n");
-        exit((0));
+        printf("\nEnter your choice\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter the required data (Emp no, Name, Dept, Desig, sal, phone)\n");
+            scanf("%s%s%s%s%d%lld", s, n, dpt, des, &sal, &p);
+            create(s, n, dpt, des, sal, p);
+            break;
+        case 2:
+            display();
+            break;
+        case 3:
+            printf("Enter the required data (Emp no, Name, Dept, Desig, sal, phone)\n");
+            scanf("%s%s%s%s%d%lld", s, n, dpt, des, &sal, &p);
+            ins_beg(s, n, dpt, des, sal, p);
+            break;
+        case 4:
+            printf("Enter the required data (Emp no, Name, Dept, Desig, sal, phone)\n");
+            scanf("%s%s%s%s%d%lld", s, n, dpt, des, &sal, &p);
+            ins_end(s, n, dpt, des, sal, p);
+            break;
+        case 5:
+            del_beg();
+            break;
+        case 6:
+            del_end();
+            break;
+        case 7:
+            exit(0);
+        }
     }
-    return X;
+
+    return 0;
 }
-NODE insert_front(student item,NODE first)
+
+void create(char s[15], char n[20], char dpt[5], char des[10], int sal, long long int p)
 {
-    NODE temp;
-    temp=getnode();
-    temp->usn=item.usn;
-    strcpy(temp->name,item.name);
-    strcpy(temp->branch,item.branch);
-   temp->semester=item.semester;
-   strcpy(temp->phone,item.phone);
-   item->link=NULL;
-   if(first==NULL)
-   return temp;
-   temp->link=first;
-return temp;
-}
-NODE insert_rear(STUDENT item,NODE first)
-{
-NODE temp,cur;
-temp=getnode();
-temp->usn=item.usn;
-strcpy(temp->name,item.name);
-strcpy(temp->branch,item.branch);
-temp->semester=item.semester;
-strcpy(temp->phone,item.phone);
-temp->link=NULL;
-if(first==NULL)
-return temp;
-   cur=first;
-   while(cur->link!=NULL)
-   {
-    cur=cur->link;
-   } 
-   cur->link=temp;
-   return first;
-}
-NODE delete_front(NODE first)
-{
-    NODE temp;
-    if(first==NULL)
+    if (head == NULL)
     {
-        printf("student list is empty \n");
-        return NULL;
+        head = (struct Enode *)malloc(1 * sizeof(struct Enode));
+        strcpy(head->ssn, s);
+        strcpy(head->name, n);
+        strcpy(head->dept, dpt);
+        strcpy(head->designation, des);
+        head->salary = sal;
+        head->phno = p;
+        head->left = NULL;
+        head->right = NULL;
+        tail = head;
     }
-    temp=first;
-    temp=temp->link;
-    printf("delete student record :USN=%d",first->usn);
-    free(first);
-    return temp;
+    else
+    {
+        temp1 = (struct Enode *)malloc(1 * sizeof(struct Enode));
+        strcpy(temp1->ssn, s);
+        strcpy(temp1->name, n);
+        strcpy(temp1->dept, dpt);
+        strcpy(temp1->designation, des);
+        temp1->salary = sal;
+        temp1->phno = p;
+        tail->right = temp1;
+        temp1->right = NULL;
+        temp1->left = tail;
+        tail = temp1;
+    }
 }
-NODE delete_rear(NODE first)
+
+void display()
 {
-    NODE cur,prev;
-    if(first==NULL)
+    temp1 = head;
+    printf("\nEmployee Details\n");
+    while (temp1 != NULL)
+    {
+        printf("\n");
+        printf("SSN: %s\nName: %s\nDept: %s\nDesig: %s\nSalary: %d\nPhone: %lld\n", temp1->ssn, temp1->name, temp1->dept, temp1->designation, temp1->salary, temp1->phno);
+        printf(" ");
+        temp1 = temp1->right;
+    }
 }
-printf("student list is empty cannot delete\n");
+
+void ins_beg(char s[15], char n[20], char dpt[5], char des[10], int sal, long long int p)
+{
+    temp1 = (struct Enode *)malloc(1 * sizeof(struct Enode));
+    strcpy(temp1->ssn, s);
+    strcpy(temp1->name, n);
+    strcpy(temp1->dept, dpt);
+    strcpy(temp1->designation, des);
+    temp1->salary = sal;
+    temp1->phno = p;
+    temp1->right = head;
+    head->left = temp1;
+    head = temp1;
+    temp1->left = NULL;
+}
+
+void ins_end(char s[15], char n[20], char dpt[5], char des[10], int sal, long long int p)
+{
+    temp1 = (struct Enode *)malloc(1 * sizeof(struct Enode));
+    strcpy(temp1->ssn, s);
+    strcpy(temp1->name, n);
+    strcpy(temp1->dept, dpt);
+    strcpy(temp1->designation, des);
+    temp1->salary = sal;
+    temp1->phno = p;
+    tail->right = temp1;
+    temp1->left = tail;
+    temp1->right = NULL;
+    tail = temp1;
+}
+
+void del_beg()
+{
+    temp1 = head->right;
+    free(head);
+    head = temp1;
+    if (head != NULL)
+    {
+        head->left = NULL;
+    }
+}
+
+void del_end()
+{
+    temp1 = tail->left;
+    free(tail);
+    tail = temp1;
+    if (tail != NULL)
+    {
+        tail->right = NULL;
+    }
+}
