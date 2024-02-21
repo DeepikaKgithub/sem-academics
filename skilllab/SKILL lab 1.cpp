ed3210500499aec1ceee1575eaa897555eb5d347
DeepikaKgithub <143379956+DeepikaKgithub@users.noreply.github.com>
2024-02-21 10:11:29 +0530

    SKILL lab 1.cpp

--- skilllab/SKILL lab 1.cpp
+++ skilllab/SKILL lab 1.cpp
@@ -21,7 +21,7 @@ void printArray(int arr[], int size) {
 }
 
 int main() {
-    int arr[] = {12, 11, 13, 5, 6};
+    int arr[] = {12, 11, 15, 5, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
     insertionSort(arr, n);
     printf("Sorted array: \n");
